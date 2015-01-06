a0115b05ae446dfb5bf3ac43f5b9cdaee108d35f
EpicKitty <EpicnessTwo@users.noreply.github.com>
2015-01-06 21:23:32 +0000

    Create SimpleHelloWorld.php

---
+++ SimpleHelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  _____       _      _  ___ _   _         
+ | ____|_ __ (_) ___| |/ (_) |_| |_ _   _ 
+ |  _| | '_ \| |/ __| ' /| | __| __| | | |
+ | |___| |_) | | (__| . \| | |_| |_| |_| |
+ |_____| .__/|_|\___|_|\_\_|\__|\__|\__, |
+       |_|                          |___/ 
+ |=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=|   
+  This is the very simple API that simply
+  prints out the words "Hello, World!" as
+  shown bellow. This is also a template
+  so that all of my other APIs call be
+  formatted the same way.
+*/
+  echo 'Hello, World!'; //Prints the words "Hello, World!"
+?>
